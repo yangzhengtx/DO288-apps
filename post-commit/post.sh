2cfc6f5846a8b17a5c9c1262a8ddb4953805f029
Student User <student@workstation.lab.example.com>
2023-02-12 19:15:27 -0500

    add post.sh

---
+++ post-commit/post.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -s -S -i -X POST http://builds-for-managers-${RHT_OCP4_DEV_USER}-post-commit.${RHT_OCP4_WILDCARD_DOMAIN}/api/builds -f -d 'developer=\${DEVELOPER}&git=\${OPENSHIFT_BUILD_SOURCE}&project=\${OPENSHIFT_BUILD_NAMESPACE}'
